9935e2feefe2fa4ef306189305a1d6666abdbbd1
tarex7 <tarex7@gmail.com>
2022-09-28 15:26:59 +0200

    CRUD done

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.app')
 
-@section('content')
+<div class="container-fluid">
+    @section('content')
 <header>
     <h1 class="text-center">Modifica post</h1>
 </header> 
 
  @include('includes.admin.posts.form')
 
+</div>
 @endsection
 
